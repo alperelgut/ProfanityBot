6e4c24256f033e1701005eea482fd73300f5a083
alperelgut <alperelgut@gmail.com>
2018-12-17 15:33:04 -0800

    Add files via upload

---
+++ sqltables.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:////Users/andpere/CDW/PycharmProjects/ProfanityBot/profanity.db',
+                       connect_args={'check_same_thread': False})
+
+# Allows us to take our objects and map to a row in the database.  Identity mapping
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+from sqlalchemy import Column, Integer, String
+
+# Import declaritve base and
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# Creating Tables
+class Profanity(Base):
+    __tablename__ = "profanity"
+
+    empid = Column(Integer, primary_key=True)
+    roomid = Column(String(50))
+    words = Column(String(50))
+
+
+class Banlist(Base):
+    __tablename__ = "banlist"
+
+    empid = Column(Integer, primary_key=True)
+    roomid = Column(String(50))
+    users = Column(String(50))
+
+
+Base.metadata.create_all(engine)
